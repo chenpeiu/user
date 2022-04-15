c16bddb376661603204d547dc07bae8a04622d28
chenpeiu <chenpeiu68@gmail.com>
2022-04-15 22:20:31 +0800

    add sort

--- pages/product.vue
+++ pages/product.vue
@@ -1,30 +1,22 @@
 <template>
   <div class="userpage flex h-screen w-screen ">
     <div class="sidebar h-screen flex-col bg-slate-200 w-full" :class="{additemright:additem}">
-      <div class="topbar2 w-full h-[8%] border-b-4 border-green-800 ">
+      <div class="topbar2 w-full h-[8%] border-b-3 border-green-800 shadow-md shadow-slate-500/30 ">
         <div class="wrap w-[95%] h-full m-auto relative">
           <NuxtLink to="/">
-            <div class="logo w-[50px] h-[50px]  m-auto pt-1">
+            <div class="logo w-[50px] h-[50px] pt-1">
               <img src="https://upload.wikimedia.org/wikipedia/zh/thumb/b/bf/Starbucks_Coffee.svg/1200px-Starbucks_Coffee.svg.png" alt="" class="w-full">
             </div>
           </NuxtLink>
-          <div class="form flex items-center absolute top-1/2 -translate-y-1/2 px-3 rounded-md border-2 border-green-800 ">
+          <!-- <div class="form flex items-center absolute top-1/2 -translate-y-1/2 px-3 rounded-md border-2 border-green-800 ">
             <fa :icon='["fas" , "magnifying-glass"]' class="text-slate-500 pr-2" />
             <input type="text" class="bg-transparent focus:outline-none py-1 text-slate-600" v-model="searchitem"/>
-          </div>
-          <div class="usericon flex items-center absolute top-1/2 -translate-y-1/2 right-0">
-            <div class="icon px-4">
-              <fa :icon='["fas" , "bell"]' class="text-green-800 text-[25px]"/>
-            </div>
-            <div class="pic bg-white w-10 h-10 rounded-full overflow-hidden border-2 border-green-800">
-              <img class="max-w-[130%] mt-[5px] -ml-[6px]" src="https://st.depositphotos.com/1269204/1219/i/600/depositphotos_12196477-stock-photo-smiling-men-isolated-on-the.jpg" alt="">
-            </div>
-          </div>
+          </div> -->
         </div>
       </div>
       <div class="midbar h-[87%] overflow-y-auto">
         <div class="wrap m-auto py-[20px] w-[95%] relative">
-          <button class="addbar p-3 px-5 bg-green-800 text-white absolute left-1/2 -translate-x-1/2 top-[20px] rounded-lg overflow-hidden min-w-max" @click="additem=true"> 
+          <button class="addbar p-3 px-5 bg-green-800 text-white absolute left-1/2 -translate-x-1/2 top-[20px] rounded-lg overflow-hidden min-w-max shadow-sm shadow-slate-300" @click="additem=true"> 
             Add New Product
           </button>
           <div class="items flex flex-col my-5 mt-14 lg:flex-row ">
@@ -60,18 +52,18 @@
           </div>
           <div class="tablebar">
             <table class="w-full rounded-xl bg-white text-center shadow overflow-hidden">
-              <thead class="bg-gray-300 ">
+              <thead class="bg-green-900/50 ">
                 <tr>
-                  <th class="border-b-2 font-bold text-slate-600 py-3">Enable</th>
-                  <th class="border-b-2 font-bold text-slate-600">Image</th>
-                  <th class="border-b-2 font-bold text-slate-600">Item</th>
-                  <th class="border-b-2 font-bold text-slate-600">Category</th>
-                  <th class="border-b-2 font-bold text-slate-600">Price</th>
-                  <th class="border-b-2 font-bold text-slate-600"></th>
+                  <th class="border-b-2 font-bold text-slate-800 py-3">Enable</th>
+                  <th class="border-b-2 font-bold text-slate-800">Image</th>
+                  <th class="border-b-2 font-bold text-slate-800 cursor-pointer" @click="changesort('content')">Item</th>
+                  <th class="border-b-2 font-bold text-slate-800 cursor-pointer" @click="changesort('category')">Category</th>
+                  <th class="border-b-2 font-bold text-slate-800 cursor-pointer" @click="changesort('origin_price')">Price</th>
+                  <th class="border-b-2 font-bold text-slate-800"></th>
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="product in productapi.data" :key="product.id">
+                <tr v-for="product in sortarray" :key="product.id">
                   <td class="text-slate-500 border-b border-gray-300 py-5">
                     <div v-if="product.enabled==true" ><fa :icon='["fas" , "circle-check"]' class="text-green-600 text-2xl"/></div>
                     <div v-if="product.enabled==false" ><fa :icon='["fas" , "circle-xmark"]' class="text-rose-500 text-2xl"/></div>
@@ -105,7 +97,7 @@
         </div>
       </div>
       <div class="footer w-full h-[5%] fixed bottom-0 relative hidden md:block">
-        <div class="absolute top-1/2 -translate-y-1/2 px-8 text-slate-800/80 text-sm">© Copyright 2022</div>
+        <div class="absolute top-1/2 -translate-y-1/2 px-8 text-slate-800/80 text-sm"></div>
       </div>
     </div>
     <div v-if="isdone==false" class="fixed w-screen h-screen bg-gray-500/50">
@@ -241,6 +233,7 @@ export default {
       additem: false,
       isshowdetail: false,
       sorttype:"",
+      issort: true,
       newproduct:{
         title: "",
         category: "",
@@ -287,6 +280,23 @@ export default {
       catch{
         return 0
       }
+    },
+    sortarray(){
+      try{
+        if(this.issort==true){
+          let result=this.productapi.data.sort((a,b)=>a[this.sorttype]-b[this.sorttype])
+          console.log(result)
+          return result
+        }
+        else{
+          let result=this.productapi.data.sort((a,b)=>b[this.sorttype]-a[this.sorttype])
+          console.log(result)
+          return result
+        }
+      }
+      catch{
+        return this.productapi.data
+      }
     }
   },
   methods:{
@@ -328,7 +338,6 @@ export default {
       })
       await this.refresh()
       this.isshowdetail=false
-
     },
     async delitem(id){
       this.isdone=false
@@ -343,6 +352,15 @@ export default {
     showdetail(id){
       this.current=this.productapi.data.find(item=>item.id==id)
       this.isshowdetail=true
+    },
+    changesort(type){
+      if (this.sorttype==type){
+        this.issort=!this.issort
+      }
+      else{
+        this.issort=!this.issort
+        this.sorttype=type
+      }
     }
   }
 }
